c4a585c7488b2497cde61c7b57c28ad92181f5d6
Nitin kumar <nitinkmr.4169@gmail.com>
2026-06-21 17:21:12 +0530

    Add Jenkins pipeline configuration to app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("Hello from Jenkins")
+pipeline {
+    agent {
+        docker {
+            image 'python:3.12-alpine'
+        }
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Run App') {
+            steps {
+                sh 'python app.py'
+            }
+        }
+    }
+}
